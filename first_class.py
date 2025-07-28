530a5718e6e7a8f6f5dd1cd8065ff5d31e28391f
amitsaxena9225 <32059302+amitsaxena9225@users.noreply.github.com>
2025-07-28 18:51:45 +0530

    This is my first commit in Project

---
+++ first_class.py
@@ -0,0 +1,11 @@
+
+def function_name (list):
+
+    for i in list:
+        if i % 2 ==0:
+            print(i,"is even")
+        else:
+            print(i,"is odd")
+
+
+function_name([1,2,3,4,5,6,7,89,10])
